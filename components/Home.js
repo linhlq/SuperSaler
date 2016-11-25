ab72c4d08196e989a62ba80c0d875ba5ac1d713e
Linh Lee <linhlq@zergitas.com>
2016-11-26 00:01:16 +0700

    Create Home screen

--- components/Home.js
+++ components/Home.js
@@ -17,13 +17,40 @@ import {
   TouchableOpacity
 } from 'react-native';
 
+var data = [
+  {name: "Dữ liệu khách hàng", color: "#C8E6C9"},
+  {name: "Lịch sử", color: "#A5D6A7"},
+  {name: "Khách của tôi", color: "#81C784"},
+  {name: "Lịch hẹn", color: "#66BB6A"},
+  {name: "Tiến độ", color: "#4CAF50"},
+  {name: "Ôn tập kiến thức", color: "#43A047"},
+  {name: "Tạo động lực", color: "#388E3C"},
+];
+
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(data),
+    };
+    this._renderRow = this._renderRow.bind(this);
+  }
+
+  _renderRow(data) {
+    return (
+      <TouchableOpacity style={{justifyContent: 'center', alignItems: 'center', height: 90, backgroundColor: data.color}}>
+        <Text style={{fontWeight: 'bold', fontSize: 20}}>{data.name}</Text>
+      </TouchableOpacity>
+    );
+  }
 
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center'}}>
-        <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
-      </View>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(data) => this._renderRow(data)}
+      />
     );
   }
 }
